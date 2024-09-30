045d5edc4c04b4677d53fc5eb9fc7d9d6b107b0d
F2598806 Ederson Franco de Lima <ederson.lima@bb.com.br>
2024-09-30 20:59:04 -0300

    Ajuste no log da inicialização

--- k8s/run-functions.sh
+++ k8s/run-functions.sh
@@ -3,9 +3,9 @@
 load_image_if_not_exists() {
   local image=$1
   if minikube image ls | grep -q "$image"; then
-    echo "$(date '+%Y-%m-%d %H:%M:%S') - Imagem $image já está presente." | tee -a $LOG_FILE
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - Imagem $image já está presente" | tee -a $LOG_FILE
   else
-    echo "$(date '+%Y-%m-%d %H:%M:%S') - Iniciando o download da imagem $image." | tee -a $LOG_FILE
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - Iniciando o download da imagem $image" | tee -a $LOG_FILE
     load_image_with_message "$image"
   fi
 }
@@ -16,16 +16,16 @@ load_image_with_message() {
     minikube image load $image &
     pid=$!
     while kill -0 $pid 2>/dev/null; do
-      echo "$(date '+%Y-%m-%d %H:%M:%S') - Baixando a imagem $image." | tee -a $LOG_FILE
+      echo "$(date '+%Y-%m-%d %H:%M:%S') - Baixando a imagem $image" | tee -a $LOG_FILE
       sleep 5
     done
     wait $pid
     if [ $? -ne 0 ]; then
-      echo "$(date '+%Y-%m-%d %H:%M:%S') - Erro ao baixar a imagem $image." | tee -a $LOG_FILE
+      echo "$(date '+%Y-%m-%d %H:%M:%S') - Erro ao baixar a imagem $image" | tee -a $LOG_FILE
       exit 1
     fi
   } 2>/dev/null
-  echo "$(date '+%Y-%m-%d %H:%M:%S') - Imagem $image baixada com sucesso." | tee -a $LOG_FILE
+  echo "$(date '+%Y-%m-%d %H:%M:%S') - Imagem $image baixada com sucesso" | tee -a $LOG_FILE
 }
 
 wait_for_resource() {
@@ -40,7 +40,7 @@ wait_for_resource() {
       echo "$(date '+%Y-%m-%d %H:%M:%S') - $success_message" | tee -a $LOG_FILE
       break
     else
-      echo "$(date '+%Y-%m-%d %H:%M:%S') - Aguardando $resource_name." | tee -a $LOG_FILE
+      echo "$(date '+%Y-%m-%d %H:%M:%S') - Aguardando $resource_name" | tee -a $LOG_FILE
       sleep 2
     fi
   done
@@ -56,7 +56,7 @@ wait_for_pod() {
       echo "$(date '+%Y-%m-%d %H:%M:%S') - $success_message" | tee -a $LOG_FILE
       break
     else
-      echo "$(date '+%Y-%m-%d %H:%M:%S') - Aguardando $selector." | tee -a $LOG_FILE
+      echo "$(date '+%Y-%m-%d %H:%M:%S') - Aguardando $selector" | tee -a $LOG_FILE
       sleep 5
     fi
   done
@@ -72,7 +72,7 @@ wait_for_hpa() {
       echo "$(date '+%Y-%m-%d %H:%M:%S') - $success_message" | tee -a $LOG_FILE
       break
     else
-      echo "$(date '+%Y-%m-%d %H:%M:%S') - Aguardando $hpa_name." | tee -a $LOG_FILE
+      echo "$(date '+%Y-%m-%d %H:%M:%S') - Aguardando $hpa_name" | tee -a $LOG_FILE
       sleep 5
     fi
   done
