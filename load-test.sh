de9a1361cdd82e6a398c981da28216c00b26fc42
F2598806 Ederson Franco de Lima <ederson.lima@bb.com.br>
2024-11-10 20:12:36 -0300

    Exclusão de arquivos

--- load-test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-end=$((SECONDS+120))
-
-while [ $SECONDS -lt $end ]; do
-  curl -X GET "http://127.0.0.1:39503/api/v2/clientes/buscar/12345678900"
-  curl -X GET "http://127.0.0.1:39503/api/v2/produtos/buscar/lanche"
-  curl -X GET "http://127.0.0.1:39503/api/v2/pedidos/listar"
-done
